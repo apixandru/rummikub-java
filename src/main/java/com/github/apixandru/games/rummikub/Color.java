9ae36aee8fc431925f16899959d81af62b15a0ea
Alexandru-Constantin Bledea <admin@notbed.com>
2015-09-16 22:30:04 +0300

    add color

---
+++ src/main/java/com/github/apixandru/games/rummikub/Color.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+package com.github.apixandru.games.rummikub;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since Sep 16, 2015
+ */
+public enum Color {
+
+	RED, BLUE, BLACK, YELLOW
+
+}
