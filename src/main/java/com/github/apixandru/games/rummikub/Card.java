c29fa2ee19626b6df7a7494ec93c4fba584a8a49
Alexandru-Constantin Bledea <admin@notbed.com>
2015-09-16 22:31:29 +0300

    add card

---
+++ src/main/java/com/github/apixandru/games/rummikub/Card.java
@@ -0,0 +1,38 @@
+/**
+ *
+ */
+package com.github.apixandru.games.rummikub;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since Sep 16, 2015
+ */
+public final class Card {
+
+	private final Color color;
+	private final Rank rank;
+
+	/**
+	 * @param color
+	 * @param rank
+	 */
+	Card(final Color color, final Rank rank) {
+		this.color = color;
+		this.rank = rank;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public Color getColor() {
+		return color;
+	}
+
+	/**
+	 * @return the rank
+	 */
+	public Rank getRank() {
+		return rank;
+	}
+
+}
