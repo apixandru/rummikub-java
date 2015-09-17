b0b105c13da10883ddf8a06a57271be633232f8c
Alexandru-Constantin Bledea <admin@notbed.com>
2015-09-17 23:51:37 +0300

    add new tests

---
+++ src/test/java/com/github/apixandru/games/rummikub/CardsTest.java
@@ -0,0 +1,46 @@
+/**
+ *
+ */
+package com.github.apixandru.games.rummikub;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since Sep 17, 2015
+ */
+@SuppressWarnings ("static-method")
+public final class CardsTest {
+
+	/**
+	 *
+	 */
+	@Test
+	public void testIsAscendingRanks() {
+		final List<Card> cards = Arrays.asList(new Card(Color.RED, Rank.ONE), new Card(Color.RED, Rank.TWO), new Card(Color.RED, Rank.THREE));
+		Assert.assertTrue("Cards should be in ascending order", Cards.isAscendingRanks(cards));
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testIsAscendingRanksWithJoker() {
+		final List<Card> cards = Arrays.asList(new Card(Color.RED, Rank.ONE), new Card(null, null), new Card(Color.RED, Rank.THREE));
+		Assert.assertTrue("Cards should be in ascending order", Cards.isAscendingRanks(cards));
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testIsAscendingRanksWithJokerBad() {
+		final List<Card> cards = Arrays.asList(new Card(Color.RED, Rank.ONE), new Card(null, null), new Card(Color.RED, Rank.FOUR));
+		Assert.assertFalse("Cards should not be in ascending order", Cards.isAscendingRanks(cards));
+	}
+
+}
