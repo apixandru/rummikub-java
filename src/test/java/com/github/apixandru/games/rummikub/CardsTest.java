9a7572086ab73b984bed30d54edcba846a4810dd
Alexandru-Constantin Bledea <admin@notbed.com>
2015-09-17 22:27:19 +0300

    add more tests

--- src/test/java/com/github/apixandru/games/rummikub/CardsTest.java
+++ src/test/java/com/github/apixandru/games/rummikub/CardsTest.java
@@ -49,4 +49,32 @@ public final class CardsTest {
 		final CardGroup group = new CardGroup(Arrays.asList(black1, blue1, red2));
 		Assert.assertFalse("A group must have different colors", group.isValid());
 	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testValidGroup() {
+		final Card black1 = new Card(Color.BLACK, Rank.ONE);
+		final Card blue1 = new Card(Color.BLUE, Rank.ONE);
+		final Card red1 = new Card(Color.RED, Rank.ONE);
+		final Card yellow1 = new Card(Color.YELLOW, Rank.ONE);
+		final CardGroup group = new CardGroup(Arrays.asList(black1, blue1, red1, yellow1));
+		Assert.assertTrue("Should be valid", group.isValid());
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testTooManyInGroup() {
+		final Card black1 = new Card(Color.BLACK, Rank.ONE);
+		final Card blue1 = new Card(Color.BLUE, Rank.ONE);
+		final Card red1 = new Card(Color.RED, Rank.ONE);
+		final Card yellow1 = new Card(Color.YELLOW, Rank.ONE);
+		final Card joker = new Card(null, null);
+		final CardGroup group = new CardGroup(Arrays.asList(black1, blue1, red1, yellow1, joker));
+		Assert.assertFalse("A group can have at most 4 cards", group.isValid());
+	}
+
 }
