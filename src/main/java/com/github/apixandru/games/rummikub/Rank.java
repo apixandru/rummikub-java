8e67767d6ec304864656bff9f71cd2731e0effdc
Alexandru-Constantin Bledea <admin@notbed.com>
2015-09-20 20:58:14 +0300

    i'm not sure if the number is actually needed

--- src/main/java/com/github/apixandru/games/rummikub/Rank.java
+++ src/main/java/com/github/apixandru/games/rummikub/Rank.java
@@ -9,16 +9,7 @@ package com.github.apixandru.games.rummikub;
  */
 public enum Rank {
 
-	ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), ELEVEN(11), TWELVE(12), THIRTEEN(13);
-
-	private final int num;
-
-	/**
-	 * @param num
-	 */
-	private Rank(final int num) {
-		this.num = num;
-	}
+	ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, ELEVEN, TWELVE, THIRTEEN;
 
 	/**
 	 * @return
