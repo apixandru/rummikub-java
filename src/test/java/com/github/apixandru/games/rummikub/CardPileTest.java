8f3ff4db7abc7eb7a03b6b8b080c23931f999e93
Alexandru-Constantin Bledea <admin@notbed.com>
2015-09-20 22:35:48 +0300

    add tests for card pile

---
+++ src/test/java/com/github/apixandru/games/rummikub/CardPileTest.java
@@ -0,0 +1,50 @@
+/**
+ *
+ */
+package com.github.apixandru.games.rummikub;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since Sep 20, 2015
+ */
+@SuppressWarnings ("static-method")
+public class CardPileTest {
+
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	public void testNumberOfCards() throws Exception {
+		final CardPile cardPile = new CardPile();
+		Assert.assertSame("Numbers of cards don't match", 54, cardPile.getNumberOfCards());
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	public void testHasCards() throws Exception {
+		final CardPile cardPile = new CardPile();
+		while (cardPile.hasMoreCards()) {
+			Assert.assertNotNull("Should have cards", cardPile.nextCard());
+		}
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	@Test (expected = NoSuchElementException.class)
+	public void testExhaustedPile() throws Exception {
+		final CardPile cardPile = new CardPile();
+		while (cardPile.hasMoreCards()) {
+			Assert.assertNotNull("Should have cards", cardPile.nextCard());
+		}
+		cardPile.nextCard();
+	}
+
+}
