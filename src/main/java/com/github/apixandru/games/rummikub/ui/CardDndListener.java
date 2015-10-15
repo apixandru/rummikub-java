49da1368ee43f66140376480bf5ce9fec3e93de4
Alexandru-Constantin Bledea <admin@notbed.com>
2015-10-15 21:21:16 +0300

    add basic drag and drop listener

---
+++ src/main/java/com/github/apixandru/games/rummikub/ui/CardDndListener.java
@@ -0,0 +1,98 @@
+/**
+ *
+ */
+package com.github.apixandru.games.rummikub.ui;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since Oct 13, 2015
+ */
+final class CardDndListener extends MouseAdapter {
+
+	private final JLayeredPane layeredPane;
+	private final JPanel panel;
+
+	private CardUi movingPiece;
+	private int xOffset;
+	private int yOffset;
+
+	CardDndListener(final JLayeredPane layeredPane, final JPanel pane) {
+		this.layeredPane = layeredPane;
+		this.panel = pane;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseAdapter#mousePressed(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void mousePressed(final MouseEvent e) {
+		final CardUi card = getCard(e);
+		if (null == card) {
+			return;
+		}
+		this.movingPiece = card;
+
+		final Point parentLocation = card.getParent().getLocation();
+		this.xOffset = parentLocation.x - e.getX();
+		this.yOffset = parentLocation.y - e.getY();
+
+		layeredPane.add(movingPiece, JLayeredPane.DRAG_LAYER);
+
+		updateMovingPieceLocation(e);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseAdapter#mouseDragged(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void mouseDragged(final MouseEvent e) {
+		if (this.movingPiece == null) {
+			return;
+		}
+		updateMovingPieceLocation(e);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseAdapter#mouseReleased(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void mouseReleased(final MouseEvent e) {
+		if (this.movingPiece == null) {
+			return;
+		}
+		final Container parent = (Container) this.panel.findComponentAt(e.getX(), e.getY());
+		parent.add(this.movingPiece);
+		this.layeredPane.repaint();
+		parent.validate();
+		this.movingPiece = null;
+	}
+
+	/**
+	 * @param e
+	 * @return
+	 */
+	private CardUi getCard(final MouseEvent e) {
+		Component c = this.panel.findComponentAt(e.getX(), e.getY());
+		while (null != c && !(c instanceof CardUi)) {
+			c = c.getParent();
+		}
+		return (CardUi) c;
+	}
+
+	/**
+	 * @param e
+	 */
+	private void updateMovingPieceLocation(final MouseEvent e) {
+		this.movingPiece.setLocation(e.getX() + xOffset, e.getY() + yOffset);
+	}
+
+}
