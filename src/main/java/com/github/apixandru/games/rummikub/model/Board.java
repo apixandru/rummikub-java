cc64f99fa8574b9190796f49ef9ef7d824039cd5
Alexandru-Constantin Bledea <admin@notbed.com>
2015-11-24 19:34:28 +0200

    extract stream for use in different methods

--- src/main/java/com/github/apixandru/games/rummikub/model/Board.java
+++ src/main/java/com/github/apixandru/games/rummikub/model/Board.java
@@ -8,6 +8,7 @@ import com.github.apixandru.games.rummikub.model.util.Util;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Alexandru-Constantin Bledea
@@ -18,11 +19,18 @@ public class Board {
     private final Card[][] cardsOnBoard = new Card[7][20];
 
     public List<CardGroup> getGroups() {
+        return streamGroups(cardsOnBoard).collect(Collectors.toList());
+    }
+
+    public boolean isValid() {
+        return streamGroups(cardsOnBoard).allMatch(CardGroup::isValid);
+    }
+
+    private Stream<CardGroup> streamGroups(Card[][] cardsOnBoard) {
         return Arrays.stream(cardsOnBoard)
                 .map(Util::splitNonEmptyGroups)
                 .flatMap(List::stream)
-                .map(CardGroup::new)
-                .collect(Collectors.toList());
+                .map(CardGroup::new);
     }
 
 }
