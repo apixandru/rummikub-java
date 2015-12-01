e33fad817dd7b7feb164e5042724c292d77b29ef
Alexandru-Constantin Bledea <admin@notbed.com>
2015-12-01 12:45:11 +0200

    add a new test

--- src/test/java/com/github/apixandru/games/rummikub/model/BoardTest.java
+++ src/test/java/com/github/apixandru/games/rummikub/model/BoardTest.java
@@ -17,4 +17,15 @@ public class BoardTest {
         board.placeCard(new Card(Color.BLUE, Rank.ONE), 2, 1);
         Assert.assertTrue(board.isValid());
     }
+
+
+    @Test
+    public void testInvalidFormation() {
+        final Board board = new Board();
+        board.placeCard(new Card(Color.BLACK, Rank.ONE), 0, 1);
+        board.placeCard(new Card(Color.RED, Rank.ONE), 3, 1);
+        board.placeCard(new Card(Color.BLUE, Rank.ONE), 2, 1);
+        Assert.assertFalse(board.isValid());
+        Assert.assertEquals(2, board.getGroups().size());
+    }
 }
