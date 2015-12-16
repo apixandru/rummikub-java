32edd807fb85be250dc020d936ae432c7777db8e
Alexandru-Constantin Bledea <admin@notbed.com>
2015-12-16 21:27:33 +0200

    reformat

--- src/main/java/com/github/apixandru/games/rummikub/ui/Main.java
+++ src/main/java/com/github/apixandru/games/rummikub/ui/Main.java
@@ -1,19 +1,10 @@
 package com.github.apixandru.games.rummikub.ui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JLayeredPane;
-import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
-
 import com.github.apixandru.games.rummikub.model.CardPile;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 /**
  * @author Alexandru-Constantin Bledea
@@ -21,66 +12,66 @@ import com.github.apixandru.games.rummikub.model.CardPile;
  */
 public final class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(final String[] args) {
-		final JFrame frame = new JFrame();
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+        final JFrame frame = new JFrame();
 
-		final JPanel grid = new JPanel();
+        final JPanel grid = new JPanel();
 
-		final JLayeredPane layeredPane = new JLayeredPane();
-		layeredPane.setPreferredSize(new Dimension(20 * 60, 7 * 96));
-		layeredPane.add(grid, JLayeredPane.DEFAULT_LAYER);
+        final JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(20 * 60, 7 * 96));
+        layeredPane.add(grid, JLayeredPane.DEFAULT_LAYER);
 
-		final CardDndListener listener = new CardDndListener(new ComponentDragSource(grid));
-		layeredPane.addMouseListener(listener);
-		layeredPane.addMouseMotionListener(listener);
+        final CardDndListener listener = new CardDndListener(new ComponentDragSource(grid));
+        layeredPane.addMouseListener(listener);
+        layeredPane.addMouseMotionListener(listener);
 
-		initializeGrid(grid, new CardPile());
+        initializeGrid(grid, new CardPile());
 
-		frame.setContentPane(centerHorizontally(layeredPane));
+        frame.setContentPane(centerHorizontally(layeredPane));
 
-		frame.pack();
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-	}
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 
-	/**
-	 * @param contentToCenter
-	 * @return
-	 */
-	private static JPanel centerHorizontally(final Component contentToCenter) {
-		final JPanel result = new JPanel();
-		result.setLayout(new BoxLayout(result, BoxLayout.X_AXIS));
-		result.add(Box.createHorizontalGlue());
-		result.add(contentToCenter);
-		result.add(Box.createHorizontalGlue());
-		return result;
-	}
+    /**
+     * @param contentToCenter
+     * @return
+     */
+    private static JPanel centerHorizontally(final Component contentToCenter) {
+        final JPanel result = new JPanel();
+        result.setLayout(new BoxLayout(result, BoxLayout.X_AXIS));
+        result.add(Box.createHorizontalGlue());
+        result.add(contentToCenter);
+        result.add(Box.createHorizontalGlue());
+        return result;
+    }
 
-	/**
-	 * @param grid
-	 * @param cardPile
-	 */
-	private static void initializeGrid(final JPanel grid, final CardPile cardPile) {
-		final int rows = 7;
-		final int cols = 20;
-		grid.setLayout(new GridLayout(rows, cols));
-		grid.setSize(new Dimension(cols * 60, rows * 96));
-		boolean first = true;
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				final JPanel square = new JPanel(new BorderLayout());
-				square.setBorder(new LineBorder(Color.LIGHT_GRAY));
-				grid.add(square);
-				if (first) {
-					first = false;
-					square.add(new CardUi(cardPile.nextCard()));
-				}
-			}
-		}
-	}
+    /**
+     * @param grid
+     * @param cardPile
+     */
+    private static void initializeGrid(final JPanel grid, final CardPile cardPile) {
+        final int rows = 7;
+        final int cols = 20;
+        grid.setLayout(new GridLayout(rows, cols));
+        grid.setSize(new Dimension(cols * 60, rows * 96));
+        boolean first = true;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                final JPanel square = new JPanel(new BorderLayout());
+                square.setBorder(new LineBorder(Color.LIGHT_GRAY));
+                grid.add(square);
+                if (first) {
+                    first = false;
+                    square.add(new CardUi(cardPile.nextCard()));
+                }
+            }
+        }
+    }
 
 }
