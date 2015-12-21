fd8f0416fc755a3371f97cef7fdc1511c0225d39
Alexandru-Constantin Bledea <admin@notbed.com>
2015-12-21 08:12:23 +0200

    add card state

--- src/main/java/com/github/apixandru/games/rummikub/model/Card.java
+++ src/main/java/com/github/apixandru/games/rummikub/model/Card.java
@@ -12,6 +12,7 @@ public final class Card {
     private final int id;
     private final Color color;
     private final Rank rank;
+    private CardState cardState;
 
     /**
      * @param id
@@ -51,4 +52,19 @@ public final class Card {
     public int getId() {
         return id;
     }
+
+    /**
+     * @return
+     */
+    public CardState getCardState() {
+        return cardState;
+    }
+
+    /**
+     * @param cardState
+     */
+    public void setCardState(final CardState cardState) {
+        this.cardState = cardState;
+    }
+
 }
---
+++ src/main/java/com/github/apixandru/games/rummikub/model/CardState.java
@@ -0,0 +1,14 @@
+package com.github.apixandru.games.rummikub.model;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since December 21, 2015
+ */
+public enum CardState {
+
+    IN_PILE,
+    IN_HAND,
+    PLACED_ON_BOARD,
+    LOCKED_ON_BOARD
+
+}
