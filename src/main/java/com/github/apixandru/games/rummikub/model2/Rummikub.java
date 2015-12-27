e08d8cbfa293240216809fb625923b70a9371c06
Alexandru-Constantin Bledea <admin@notbed.com>
2015-12-27 14:25:43 +0200

    wrong callback

--- src/main/java/com/github/apixandru/games/rummikub/model2/Rummikub.java
+++ src/main/java/com/github/apixandru/games/rummikub/model2/Rummikub.java
@@ -17,6 +17,6 @@ public interface Rummikub {
      * @param callback
      * @return
      */
-    Player addPlayer(String name, RummikubFactory callback);
+    Player addPlayer(String name, RummikubCallback callback);
 
 }
--- src/main/java/com/github/apixandru/games/rummikub/model2/RummikubImpl.java
+++ src/main/java/com/github/apixandru/games/rummikub/model2/RummikubImpl.java
@@ -79,7 +79,7 @@ final class RummikubImpl implements Rummikub {
     }
 
     @Override
-    public Player addPlayer(final String name, final RummikubFactory callback) {
+    public Player addPlayer(final String name, final RummikubCallback callback) {
         final PlayerImpl player = new PlayerImpl(listener, getCards(14));
         this.players.add(player);
         if (null == this.currentPlayer) {
