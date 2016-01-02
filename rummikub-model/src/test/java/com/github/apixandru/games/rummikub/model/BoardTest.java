606c818855d0a5ffd753973866e82e8360ec9ce9
Alexandru-Constantin Bledea <admin@notbed.com>
2016-01-02 20:16:23 +0200

    increase code coverage on the board

--- rummikub-model/src/test/java/com/github/apixandru/games/rummikub/model/BoardTest.java
+++ rummikub-model/src/test/java/com/github/apixandru/games/rummikub/model/BoardTest.java
@@ -25,6 +25,9 @@ public final class BoardTest {
         board = new Board(null);
     }
 
+    /**
+     * A group of ones placed one next to another is a valid formation.
+     */
     @Test
     public void testValidFormation() {
         board.placeCard(card(BLACK, ONE), 0, 1);
@@ -33,13 +36,44 @@ public final class BoardTest {
         assertTrue(board.isValid());
     }
 
+    /**
+     * A group of ones placed one next to another with a gap is not a valid formation.
+     */
     @Test
     public void testInvalidFormation() {
-        board.placeCard(card(BLACK, ONE), 0, 6);
+        board.placeCard(card(BLACK, ONE), 16, 6);
         board.placeCard(card(RED, ONE), 18, 6);
         board.placeCard(card(BLUE, ONE), 19, 6);
         assertFalse(board.isValid());
         assertEquals(2, board.streamGroups().count());
+    }
+
+    /**
+     * Should not be able to place cards out of bounds
+     */
+    @Test
+    public void testPlaceOutOfBounds() {
+        assertFalse(board.placeCard(card(BLACK, ONE), 0, 7));
+        assertFalse(board.placeCard(card(BLACK, ONE), 20, 0));
+    }
+
+    /**
+     * Should not be able to place different cards on the same place.
+     */
+    @Test
+    public void testPlaceCardOnTakenSpot() {
+        assertTrue(board.placeCard(card(BLACK, ONE), 0, 6));
+        assertFalse(board.placeCard(card(BLACK, ONE), 0, 6));
+    }
+
+    /**
+     * Should be able to place the same cards in the same slot.
+     */
+    @Test
+    public void testPlaceSameCardOnTakenSpot() {
+        final Card card = card(BLACK, ONE);
+        assertTrue(board.placeCard(card, 0, 6));
+        assertTrue(board.placeCard(card, 0, 6));
     }
 
 }
