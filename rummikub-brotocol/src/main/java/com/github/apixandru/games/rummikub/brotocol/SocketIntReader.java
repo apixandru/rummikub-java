ae141aa2df1e2920aa923522929f40be0f6907e4
Alexandru-Constantin Bledea <admin@notbed.com>
2016-01-04 21:16:03 +0200

    add aux classes

---
+++ rummikub-brotocol/src/main/java/com/github/apixandru/games/rummikub/brotocol/SocketIntReader.java
@@ -0,0 +1,36 @@
+package com.github.apixandru.games.rummikub.brotocol;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since January 04, 2016
+ */
+public class SocketIntReader implements IntReader {
+
+    private final Socket socket;
+    private final ObjectInputStream in;
+
+    /**
+     * @param socket
+     */
+    public SocketIntReader(final Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+    }
+
+    @Override
+    public int readInt() throws IOException {
+        return this.in.readInt();
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.in.close();
+        this.socket.close();
+    }
+
+}
---
+++ rummikub-brotocol/src/main/java/com/github/apixandru/games/rummikub/brotocol/SocketIntWriter.java
@@ -0,0 +1,45 @@
+package com.github.apixandru.games.rummikub.brotocol;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since January 04, 2016
+ */
+public class SocketIntWriter implements IntWriter {
+
+    private final Socket socket;
+    private final ObjectOutputStream out;
+
+    /**
+     * @param socket
+     */
+    public SocketIntWriter(final Socket socket) throws IOException {
+        this.socket = socket;
+        this.out = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+    }
+
+    @Override
+    public void write(final int... ints) {
+        try {
+            for (int oneInt : ints) {
+                this.out.writeInt(oneInt);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void flush() {
+        try {
+            this.out.flush();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+}
