d212b1a021901cc5dad429f6afdc3385efe9d9eb
Alexandru-Constantin Bledea <admin@notbed.com>
2016-01-05 08:13:24 +0200

    implement brotocol

--- rummikub-server/src/main/java/com/github/apixandru/games/rummikub/server/ClientRunnable.java
+++ rummikub-server/src/main/java/com/github/apixandru/games/rummikub/server/ClientRunnable.java
@@ -6,10 +6,16 @@ import com.github.apixandru.games.rummikub.brotocol.util.AbstractIntWritable;
 import com.github.apixandru.games.rummikub.model.Card;
 import com.github.apixandru.games.rummikub.model.Player;
 import com.github.apixandru.games.rummikub.model.PlayerCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.List;
 
+import static com.github.apixandru.games.rummikub.brotocol.Brotocol.CLIENT_END_TURN;
+import static com.github.apixandru.games.rummikub.brotocol.Brotocol.CLIENT_MOVE_CARD;
+import static com.github.apixandru.games.rummikub.brotocol.Brotocol.CLIENT_PLACE_CARD;
+import static com.github.apixandru.games.rummikub.brotocol.Brotocol.CLIENT_TAKE_CARD;
 import static com.github.apixandru.games.rummikub.brotocol.Brotocol.SERVER_CARD_PLACED;
 import static com.github.apixandru.games.rummikub.brotocol.Brotocol.SERVER_CARD_REMOVED;
 import static com.github.apixandru.games.rummikub.brotocol.Brotocol.SERVER_RECEIVED_CARD;
@@ -19,6 +25,8 @@ import static com.github.apixandru.games.rummikub.brotocol.Brotocol.SERVER_RECEI
  * @since January 04, 2016
  */
 public class ClientRunnable extends AbstractIntWritable implements Runnable, PlayerCallback<Integer>, Player<Integer> {
+
+    private static final Logger log = LoggerFactory.getLogger(ClientRunnable.class);
 
     private final IntReader intReader;
 
@@ -38,11 +46,44 @@ public class ClientRunnable extends AbstractIntWritable implements Runnable, Pla
         try (IntReader reader = intReader) {
             while (true) {
                 final int input = reader.readInt();
-                System.out.println(input);
+                switch (input) {
+                    case CLIENT_PLACE_CARD:
+                        handleOnCardPlacedOnBoard(reader);
+                        break;
+                    case CLIENT_END_TURN:
+                        endTurn();
+                        break;
+                    case CLIENT_TAKE_CARD:
+                        takeCardFromBoard(readCard(reader), reader.readInt(), reader.readInt(), reader.readInt());
+                        break;
+                    case CLIENT_MOVE_CARD:
+                        moveCardOnBoard(readCard(reader), reader.readInt(), reader.readInt(), reader.readInt(), reader.readInt());
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Unknown input: " + input);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void handleOnCardPlacedOnBoard(final IntReader reader) throws IOException {
+        log.info("Received on onCardPlacedOnBoard request.");
+        final Card card = readCard(reader);
+        final int x = reader.readInt();
+        final int y = reader.readInt();
+        log.info("Params: Card={}, x={}, y={}", card, x, y);
+        onCardPlacedOnBoard(card, x, y);
+    }
+
+    /**
+     * @param reader
+     * @return
+     * @throws IOException
+     */
+    private Card readCard(final IntReader reader) throws IOException {
+        return this.cards.get(reader.readInt());
     }
 
     @Override
