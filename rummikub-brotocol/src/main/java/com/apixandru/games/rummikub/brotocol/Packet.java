af7daddba6bf3429deb7feeabc4f004e4342675a
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-03-28 20:12:50 +0300

    packet shouldn't be java serializable

--- rummikub-brotocol/src/main/java/com/apixandru/games/rummikub/brotocol/Packet.java
+++ rummikub-brotocol/src/main/java/com/apixandru/games/rummikub/brotocol/Packet.java
@@ -1,11 +1,9 @@
 package com.apixandru.games.rummikub.brotocol;
 
-import java.io.Serializable;
-
 /**
  * @author Alexandru-Constantin Bledea
  * @since January 13, 2016
  */
-public interface Packet extends Serializable {
+public interface Packet {
 
 }
