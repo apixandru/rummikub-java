5cd9e09a030de56c860f01d2edd55ee5f639c79c
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-04-01 08:09:59 +0300

    add waiting room listener

--- rummikub-ui-swing/src/main/java/com/apixandru/games/rummikub/swing/WaitingRoom.java
+++ rummikub-ui-swing/src/main/java/com/apixandru/games/rummikub/swing/WaitingRoom.java
@@ -1,6 +1,7 @@
 package com.apixandru.games.rummikub.swing;
 
 import javax.swing.Box;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
@@ -12,28 +13,36 @@ import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import static java.awt.BorderLayout.CENTER;
+import static java.awt.BorderLayout.EAST;
+import static java.awt.GridBagConstraints.HORIZONTAL;
+import static java.awt.GridBagConstraints.NORTHWEST;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /**
  * @author Alexandru-Constantin Bledea
  * @since March 29, 2016
  */
-public class WaitingRoom extends JPanel {
+public class WaitingRoom extends JPanel implements WaitingRoomListener {
 
     private final JButton btnStart = new JButton("Start");
+
+    private final DefaultListModel<String> playerListModel = new DefaultListModel<>();
 
     private WaitingRoom() {
         super(new BorderLayout());
 
-        final JList<String> list = new JList<>();
+        final JList<String> list = new JList<>(playerListModel);
         list.setVisibleRowCount(6);
         list.setFixedCellWidth(150);
 
-        add(new JScrollPane(list), BorderLayout.CENTER);
-        add(createButtonPane(), BorderLayout.EAST);
+        add(new JScrollPane(list), CENTER);
+        add(createButtonPane(), EAST);
     }
 
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("Waiting room");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setContentPane(new WaitingRoom());
 
         frame.pack();
@@ -50,8 +59,8 @@ public class WaitingRoom extends JPanel {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.NORTHWEST;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = NORTHWEST;
+        gbc.fill = HORIZONTAL;
 
         gbc.weighty = 1;
         gbc.weightx = 1;
@@ -63,6 +72,16 @@ public class WaitingRoom extends JPanel {
         buttonPane.add(btnStart, gbc);
         buttonPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         return buttonPane;
+    }
+
+    @Override
+    public void playerJoined(final String playerName) {
+        playerListModel.addElement(playerName);
+    }
+
+    @Override
+    public void playerLeft(final String playerName) {
+        playerListModel.removeElement(playerName);
     }
 
 }
---
+++ rummikub-ui-swing/src/main/java/com/apixandru/games/rummikub/swing/WaitingRoomListener.java
@@ -0,0 +1,13 @@
+package com.apixandru.games.rummikub.swing;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since April 01, 2016
+ */
+public interface WaitingRoomListener {
+
+    void playerJoined(String playerName);
+
+    void playerLeft(String playerName);
+
+}
