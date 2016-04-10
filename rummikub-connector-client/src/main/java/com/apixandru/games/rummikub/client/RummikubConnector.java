3aae42a8cb311c59749f14c58d6d28164877a003
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-04-10 14:47:01 +0300

    remove unused code

--- rummikub-connector-client/src/main/java/com/apixandru/games/rummikub/client/RummikubConnector.java
+++ rummikub-connector-client/src/main/java/com/apixandru/games/rummikub/client/RummikubConnector.java
@@ -1,13 +1,7 @@
 package com.apixandru.games.rummikub.client;
 
 import com.apixandru.games.rummikub.brotocol.SocketWrapper;
-import com.apixandru.games.rummikub.client.waiting.StartGameListenerImpl;
 import com.apixandru.rummikub.StateChangeListener;
-import com.apixandru.rummikub.waiting.StartGameListener;
-import com.apixandru.rummikub.waiting.WaitingRoomListener;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Alexandru-Constantin Bledea
@@ -18,19 +12,9 @@ public final class RummikubConnector<H> {
     final SocketWrapper socketWrapper;
     final StateChangeListener<H> stateChangeListener;
 
-    private final List<WaitingRoomListener> waitingRoomListeners = new ArrayList<>();
-
     public RummikubConnector(final SocketWrapper socketWrapper, final StateChangeListener<H> stateChangeListener) {
         this.socketWrapper = socketWrapper;
         this.stateChangeListener = stateChangeListener;
-    }
-
-    public void addWaitingRoomListener(final WaitingRoomListener waitingRoomListener) {
-        this.waitingRoomListeners.add(waitingRoomListener);
-    }
-
-    public StartGameListener newStartGameListener() {
-        return new StartGameListenerImpl(this.socketWrapper);
     }
 
 }
