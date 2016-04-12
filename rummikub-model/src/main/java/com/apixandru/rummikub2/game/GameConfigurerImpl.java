818d0a830e7f2849f97edefad81429423171337e
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-04-12 19:40:55 +0300

    game configurer

---
+++ rummikub-model/src/main/java/com/apixandru/rummikub2/game/GameConfigurerImpl.java
@@ -0,0 +1,34 @@
+package com.apixandru.rummikub2.game;
+
+import com.apixandru.games.rummikub.api.BoardCallback;
+import com.apixandru.games.rummikub.api.GameEventListener;
+import com.apixandru.games.rummikub.api.Player;
+import com.apixandru.games.rummikub.api.PlayerCallback;
+import com.apixandru.games.rummikub.model.Rummikub;
+import com.apixandru.games.rummikub.model.RummikubFactory;
+import com.apixandru.rummikub.game.GameConfigurer;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since April 11, 2016
+ */
+public class GameConfigurerImpl implements GameConfigurer<Integer> {
+
+    private final Rummikub<Integer> rummikubGame = RummikubFactory.newInstance();
+
+    @Override
+    public void addGameEventListener(final GameEventListener gameEventListener) {
+        rummikubGame.addGameEventListener(gameEventListener);
+    }
+
+    @Override
+    public void addBoardCallback(final BoardCallback boardCallback) {
+        rummikubGame.addBoardCallback(boardCallback);
+    }
+
+    @Override
+    public Player<Integer> newPlayer(final PlayerCallback<Integer> playerCallback) {
+        return rummikubGame.addPlayer(playerCallback.getPlayerName(), playerCallback);
+    }
+
+}
