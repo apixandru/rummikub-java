668c24af02d24f567067567835b33c59760205aa
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-04-21 18:15:54 +0300

    add card slot player

---
+++ rummikub-ui-swing/src/main/java/com/apixandru/games/rummikub/swing/CardSlotPlayer.java
@@ -0,0 +1,47 @@
+package com.apixandru.games.rummikub.swing;
+
+import com.apixandru.games.rummikub.api.Card;
+import com.apixandru.games.rummikub.api.Player;
+
+import java.util.List;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since Apr 21, 2016
+ */
+public class CardSlotPlayer implements Player<CardSlot> {
+
+    private final Player<Integer> player;
+    private final List<CardSlot> cardSlots;
+
+    public CardSlotPlayer(final Player<Integer> player, final List<CardSlot> cardSlots) {
+        this.player = player;
+        this.cardSlots = cardSlots;
+    }
+
+    @Override
+    public String getName() {
+        return player.getName();
+    }
+
+    @Override
+    public void placeCardOnBoard(final Card card, final int x, final int y) {
+        player.placeCardOnBoard(card, x, y);
+    }
+
+    @Override
+    public void moveCardOnBoard(final Card card, final int fromX, final int fromY, final int toX, final int toY) {
+        player.moveCardOnBoard(card, fromX, fromY, toX, toY);
+    }
+
+    @Override
+    public void takeCardFromBoard(final Card card, final int x, final int y, final CardSlot hint) {
+        player.takeCardFromBoard(card, x, y, cardSlots.indexOf(hint));
+    }
+
+    @Override
+    public void endTurn() {
+        player.endTurn();
+    }
+
+}
