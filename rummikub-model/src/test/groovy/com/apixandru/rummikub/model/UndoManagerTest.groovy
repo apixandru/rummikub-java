503a6832229a4cfd6fbe0b7bedc09906a6907572
Alexandru-Constantin Bledea <contact@apixandru.com>
2017-02-16 19:22:48 +0200

    add test for undo manager

---
+++ rummikub-model/src/test/groovy/com/apixandru/rummikub/model/UndoManagerTest.groovy
@@ -0,0 +1,34 @@
+package com.apixandru.rummikub.model
+
+import com.apixandru.rummikub.api.Card
+import spock.lang.Specification
+
+import static com.apixandru.rummikub.model.TestUtils.BLACK_ONE_1
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since February 19, 2017
+ */
+class UndoManagerTest extends Specification {
+
+    UndoManager undoManager;
+
+    def "should place card back on board"() {
+        given:
+        def player = Mock(EnhancedPlayer)
+        def board = Mock(Board)
+        undoManager.trackBoardToPlayer(BLACK_ONE_1, 1, 1)
+
+        when:
+        undoManager.undo(player, board)
+
+        then:
+        board.getCards() >> []
+        1 * board.placeCard(BLACK_ONE_1, 1, 1)
+    }
+
+    def setup() {
+        undoManager = new UndoManager();
+    }
+
+}
