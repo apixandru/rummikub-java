3c3154d79c87f561b4691de89ae971d16a663d3a
Alexandru-Constantin Bledea <contact@apixandru.com>
2017-08-12 16:22:48 +0300

    extract card slot into component

---
+++ rummikub-angular/src/app/app.cardslot.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-slot',
+  templateUrl: './app.cardslot.html'
+})
+export class AppCardSlot {
+}
--- rummikub-angular/src/app/app.module.ts
+++ rummikub-angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppCardSlot } from "./app.cardslot";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppCardSlot
   ],
   imports: [
     BrowserModule
