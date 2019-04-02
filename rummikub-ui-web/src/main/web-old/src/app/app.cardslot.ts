583234c95e4146170e3eedb7bdfd956603b4c386
Alexandru Bledea <contact@apixandru.com>
2019-04-01 22:34:34 -0400

    simplify the javascript app

--- rummikub-ui-web/src/main/web-old/src/app/app.cardslot.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'card-slot',
-  templateUrl: './app.cardslot.html'
-})
-export class AppCardSlot {
-  @Input() cardSlotId: any;
-}
